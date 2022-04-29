df8cf30916e8208d0e4a97471b3a449aa42fa90b
MarkmanCZ <78849760+MarkmanCZ@users.noreply.github.com>
2022-04-29 15:25:02 +0200

    update

---
+++ classes/class.config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Config
+{
+    public static $host = "pma.markman.cz";
+    public static $user = "komponenty";
+    public static $pwd = "K0mp@2022!projekt";
+    public static $db = "komponenty";
+}
--- classes/class.database.php
+++ classes/class.database.php
@@ -3,15 +3,12 @@
 class Database
 {
 
-    private $host = "pma.markman.cz";
-    private $user = "komponenty";
-    private $pwd = "K0mp@2022!projekt";
-    private $db = "komponenty";
+
 
     protected $conn;
 
     public function __construct() {
-        $this->conn = new mysqli($this->host, $this->user, $this->pwd, $this->db);
+        $this->conn = new mysqli(Config::$host, Config::$user, Config::$pwd, Config::$db;
         if(!$this->conn) {
             die("Error: " . mysqli_connect_error());
         }
